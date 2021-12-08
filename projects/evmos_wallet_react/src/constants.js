833d9db29e7cd21ba929762e79409a180e7e231d
Emile Cohen <emilecohen@Emiles-MacBook-Pro.local>
2021-12-07 21:24:22 -0800

    Misc Fix

--- projects/evmos_wallet_react/src/constants.js
+++ projects/evmos_wallet_react/src/constants.js
@@ -1,7 +1,6 @@
 const FAUCET_MNEMONIC = "peace elbow health split six fold audit couple exact bean solution lobster give birth cloud uniform walk leaf save stadium doll sock fall cause"
 const PRIVATE_KEY = "0xf5c2c5ad51df662c23be107dfd100fe0166ca7870bd83698b7bb15e769065f93"
-const RPC_ENDOINT = "http://localhost:8545"
-const ALCHEMY_KEY_RINKEBY = "r1tmJPYocwv5RWRWED824cwxoDrz2DWL"
+const RPC_ENDOINT = "http://ec2-3-17-133-24.us-east-2.compute.amazonaws.com:8545"
 const ADDRESS_OTT_CONTRACT = '0x5052D35de7697B0aCF2F9F31BE8367c803d88357'
 
 const NFT_CONTRACT_MAP = [
@@ -17,5 +16,4 @@ export {
     FAUCET_MNEMONIC,
     PRIVATE_KEY,
     RPC_ENDOINT,
-    ALCHEMY_KEY_RINKEBY,
     NFT_CONTRACT_MAP};
